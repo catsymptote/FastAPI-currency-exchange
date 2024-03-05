3c318f39a72ccdb054899b05f8c5a0959fddae93
Paul Knutson <31307312+catsymptote@users.noreply.github.com>
2024-03-05 18:54:45 +0100

    Added info, all, rate and convert

--- main.py
+++ main.py
@@ -1,7 +1,60 @@
+import json
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
+# Load data
+with open('data.json', 'r') as file:
+    text = file.read()
+    data = json.loads(text)
+# print(data['rates']['NOK'])
+
+
+# Support function
+def get_rate_from_data(currency: str):
+    if currency in data['rates']:
+        return data['rates'][currency]
+    return None
+
+
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+async def get_all():
+    return {'Endpoint': 'Description',
+        '/': 'Get info on how to use.',
+        '/all': 'List all internal data, including all rates.',
+        '/rate/c': 'Get the currency-to-USD ratio. (c = currency code.)',
+        '/convert/c1,c2,amount': 'Convert the amount of c1 into c2.'
+    }
+
+
+@app.get("/all")
+async def get_all():
+    return data
+
+
+@app.get("/rate/{currency}")
+async def get_rate(currency: str):
+    # Because USD doesn't work for some reason?
+    rate = get_rate_from_data(currency)
+
+    # Construct and return message.
+    if rate is not None:
+        message = {'currency': currency, 'rate': rate}
+    else:
+        message = {'error': 'Did not work. Probably non-existent currency.'}
+    return message
+
+
+@app.get("/convert/{currency1},{currency2},{amount}")
+async def convert_currency(currency1: str, currency2: str, amount: int):
+    rate1 = get_rate_from_data(currency1)
+    rate2 = get_rate_from_data(currency2)
+    in_usd = amount / rate1
+    in_currency2 = in_usd * rate2
+    return {'money': in_currency2}
+
+
+if __name__ == '__main__':
+    import subprocess
+    subprocess.run(["hypercorn", "main:app", "--reload"])
